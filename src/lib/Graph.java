b42930614119ab1ac890193942485d7f8a31685c
Pedro Ferreira <ferfonhp@gmail.com>
2025-05-27 14:56:19 -0300

    função addVertice

--- src/lib/Graph.java
+++ src/lib/Graph.java
@@ -6,4 +6,9 @@ public class Graph<T>{
     private ArrayList<Edge> edges;
     private ArrayList<Vertex<T>> vertices;
 
+    private Vertex<T> addVertex(T value){
+        Vertex<T> novo = new Vertex<T>(value);
+        this.vertices.add(novo);
+        return novo;
+    }    
 }
